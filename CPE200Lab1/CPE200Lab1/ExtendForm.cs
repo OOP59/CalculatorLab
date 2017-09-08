85d2b007810720e483356077b2650b597b79fbf3
Teeramet Eakwilai <ppluz2540@gmail.com>
2017-09-08 16:31:07 +0700

    Almost complete , but can't more operate !!!

--- CPE200Lab1/CPE200Lab1/ExtendForm.cs
+++ CPE200Lab1/CPE200Lab1/ExtendForm.cs
@@ -15,12 +15,12 @@ namespace CPE200Lab1
         private bool isNumberPart = false;
         private bool isContainDot = false;
         private bool isSpaceAllowed = false;
-        private CalculatorEngine engine;
+        private RPN_Calculator_engine engine;
 
         public ExtendForm()
         {
             InitializeComponent();
-            engine = new CalculatorEngine();
+            engine = new RPN_Calculator_engine();
         }
 
         private bool isOperator(char ch)
